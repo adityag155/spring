d1273c0610928074b93b2afba1b4bb16045cca36
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-08-12 10:12:15 +0530

    Add files via upload

---
+++ StudentService.java
@@ -0,0 +1,10 @@
+package org.javapatil.adityaproject.service;
+
+import java.util.List;
+
+import org.javapatil.adityaproject.model.StudentForm;
+
+public interface StudentService {
+
+	List<StudentForm> getStudListService();
+}
