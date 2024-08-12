b800255741cde24b4bd240972490e986e6c5f1d6
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-08-12 10:11:08 +0530

    Add files via upload

---
+++ StudentDao.java
@@ -0,0 +1,11 @@
+package org.javapatil.adityaproject.dao;
+
+import java.util.List;
+
+import org.javapatil.adityaproject.model.StudentForm;
+
+public interface StudentDao {
+	
+	List<StudentForm> getStudList();
+	
+}
