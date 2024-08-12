527fd95db84a660b7ada3f487741fb87a64a226e
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-08-12 10:11:57 +0530

    Add files via upload

---
+++ StudentForm.java
@@ -0,0 +1,19 @@
+package org.javapatil.adityaproject.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentForm {
+	
+	private int sno;
+	private String sname;
+	private int age;
+	
+
+}
